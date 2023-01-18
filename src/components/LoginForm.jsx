3904be7e684a7df3c987dfddbce243be628c2021
Travis <56279081+travisL6@users.noreply.github.com>
2023-01-18 11:59:59 -0600

    Sign up functionality added
     Validation still needs some work

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,14 +7,13 @@ const LoginForm = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [avatar, setAvatar] = useState('');
     const [error, setError] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
         const authObject = { 'Project-ID': "0790574b-3bb8-418a-8302-9190b3d695cc", 'User-Name': username, 'User-Secret': password}
-        const authObject2 = { 'Private-key': "c6508b4a-e313-4618-8425-2d17655d2a34"}
-
+        
         try {
             await axios.get('https://api.chatengine.io/chats', { headers: authObject })
             .then(() => {
@@ -22,49 +21,50 @@ const LoginForm = () => {
                 localStorage.setItem('password', password);
                 window.location.reload();
             })
-            .catch(() =>{
-                let formData = new FormData();
-                formData.append('firstName', firstName);
-                formData.append('lastName', lastName);
-                formData.append('email', email);
-                formData.append('username', username);
-                formData.append('secret', password);
-                axios.post('https://api.chatengine.io/users', formData, { headers: authObject2 })
-                .then(() => {
-                    localStorage.setItem('username', username);
-                    localStorage.setItem('password', password);
-                    window.location.reload();
-                })
-                .catch((error) =>{
-                    console.log(error);
-                })
-            })
         } catch (error) {
             setError('Incorrect username or password. Please try again.')
         }
     }
-    function handleSignUp() {
-        document.getElementById('email').style.display = "inherit";
-        document.getElementById('firstName').style.display = "inherit";
-        document.getElementById('log-in-button').style.display = "none";
-        document.getElementById('back-button').style.display = "inherit";
-    } 
+
+    function goToSignUp() {
+        document.getElementById('sign-up-form').style.display = "inherit";
+        document.getElementById('initialSignUp').style.display = "none";
+        document.getElementById('log-in-form').style.display = "none";
+    }
+
+    const handleSignUpCompletion = async (event) => {
+        event.preventDefault();
+        const authKey = { 'Private-key': "c6508b4a-e313-4618-8425-2d17655d2a34"}
+        let formData = new FormData();
+        
+        formData.append('username', username);
+        formData.append('secret', password);
+        formData.append('email', email);
+        formData.append('first_name', firstName);
+        formData.append('last_name', lastName);
+        formData.append('custom_json', avatar);
+        try {
+            axios.post('https://api.chatengine.io/users', formData, { headers: authKey })
+            .then(() => {
+                localStorage.setItem('username', username);
+                localStorage.setItem('password', password);
+                window.location.reload();
+            })
+        } catch (error) {
+            setError('Please try again.')
+        }
+    }
 
     function handleBack() {
-        document.getElementById('email').style.display = "none";
-        document.getElementById('firstName').style.display = "none";
-        document.getElementById('log-in-button').style.display = "inherit";
-        document.getElementById('back-button').style.display = "none";
-    } 
-
+        document.getElementById('sign-up-form').style.display = "none";
+        document.getElementById('initialSignUp').style.display = "inherit";
+        document.getElementById('log-in-form').style.display = "inherit";
+    }
     return (
         <div className="wrapper">
             <div className="form">
                 <h1 className="title">Chat App</h1>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input" id="email" placeholder="Email" style={{ display: "none" }} required />
-                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="input" id="firstName" placeholder="First Name" style={{ display: "none" }} required />
-                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className="input" id="lastName" placeholder="Last Name" style={{ display: "none" }} required />
+                <form id="log-in-form" onSubmit={handleSubmit}>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" id="user" placeholder="Username" required />
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" id="pass" placeholder="Password" required />
                     <div align="center">
@@ -72,20 +72,32 @@ const LoginForm = () => {
                             <span>Log In</span>
                         </button>
                     </div>
-                    <h2 className="error">
-                        {error}
-                    </h2>
+                </form>
+                <form id="sign-up-form" onSubmit={handleSignUpCompletion} style={{display: "none"}}>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="input" id="email" placeholder="Email" required />
+                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="input" id="firstName" placeholder="First Name" required />
+                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className="input" id="lastName" placeholder="Last Name" required />
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" id="user" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" id="pass" placeholder="Password" required />
+                    <label id="avatar-label">Choose a profile picture:</label>
+                    <input type="file" value={avatar} onChange={(e) => setAvatar(e.target.value)} className="label" id="avatar" name="avatar" accept="image/png, image/jpeg"/>
+                    <div align="center">
+                        <button id="back-button" className = 'button' onClick={handleBack}>Back</button>
+                        <button type="submit" id="finalSignUp" className='button' onClick={handleSignUpCompletion}>
+                            <span>Sign Up</span>
+                        </button>
+                    </div>
                 </form>
                 <div align="center">
-                    <button id="back-button" onClick={handleBack} style={{ display: "none" }}>
-                        <span>Back</span>
-                    </button>
-                    <button className='button' onClick={handleSignUp} >
+                    <button id="initialSignUp" className='button' onClick={goToSignUp}>
                         <span>Sign Up</span>
                     </button>
                 </div>
+                <h2 className="error">
+                        {error}
+                </h2>
                 <p align="center">Made by Travis Lambert</p>
-                <p align="center"><a href="https://www.linkedin.com/in/travis-lambert-450261195/"><i class="fa-brands fa-linkedin"></i></a><a href="https://github.com/travisL6"><i class="fa-brands fa-github"></i></a></p>
+                <p align="center"><a href="https://www.linkedin.com/in/travis-lambert-450261195/"><i className="fa-brands fa-linkedin"></i></a><a href="https://github.com/travisL6"><i className="fa-brands fa-github"></i></a></p>
             </div>
         </div>
     )
